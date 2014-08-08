74a1f61e5dae72f57123c64f4fb53f56ea70396d
Nicolas Hillegeer <nicolas@hillegeer.com>
2014-08-08 11:25:23 +0200

    first code commit

--- README.md
+++ README.md
@@ -1,4 +1,73 @@
-utee
+microtee
+========
+
+Based of a [small implementation of
+tee(7)](http://lwn.net/Articles/179434/) with the
+[tee()](http://man7.org/linux/man-pages/man2/tee.2.html)/[splice()](http://man7.org/linux/man-pages/man2/splice.2.html)
+system calls by Jens Axboe.
+
+That toy example could only handle cases where both stdin and stdout
+were pipes. The reason for this being that the `tee` system call only
+accepts pipes as input (splice() only needs one of the fds to be pipes).
+
+**microtee** tries to alleviate this problem by falling back to an
+intermediate pipe if it has to deal with input or ouput that's not a
+pipe.
+
+It's just a small experiment of mine because I wanted to get to know
+tee() and splice() better. I don't expect it will see any actual
+production use. I haven't even looked at the original `tee(7)`
+implementation to see if it actually used `tee()`/`splice()`. It's
+possible, but those system calls only exist on rather modern linux
+(2.16.17) so perhaps not. Linus more or less predicted that there would
+be few users of these system calls, even if they were really awesome.
+
+Advantages:
+- No user space buffers
+  - This should be fast
+  - Should be able to use the underlying devices' DMA engine
+- Just a single tiny source file
+
+Disadvantages:
+- Not portable to other UNIXes until they implement `tee()`/`splice()`
+
+Building
+========
+
+It could be as simple as:
+
+```bash
+$ gcc utee.c -o utee && ./utee echo.txt
+```
+
+Or you could use the Makefile I'll supply in a few commits.
+
+Requirements
+============
+
+These are minimum version, though I advise to use something more recent
+as the initial implementations had bugs:
+
+- Linux kernel >= 2.6.17
+- Glibc >= 2.5
+
+TODO
 ====
 
-A tee utility that uses only tee() and splice()
+- Get rid of those `VERBOSE` ifdefs, they ugly up the codebase. I just
+  put them in so it would be more like tee(1).
+- Try `fcntl(fd, F_SETPIPE_SZ, ...)` and see if it can do something for
+  utee.
+
+Resources
+=========
+
+Things that talk about `tee()` and `splice()` or zero-copy that I found online.
+
+- [Archive of things Linus has said about tee() and
+  splice()](http://yarchive.net/comp/linux/splice.html)
+- [Some more things Linus
+  said](https://web.archive.org/web/20130521163124/http://kerneltrap.org/node/6505)
+  may overlap slightly with the link above
+- [Zero-Copy with sendfile and
+  splice](http://blog.superpat.com/2010/06/01/zero-copy-in-linux-with-sendfile-and-splice/)
---
+++ utee.c
@@ -0,0 +1,223 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdbool.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+
+static bool spliceall(int infd, int outfd, size_t len) {
+    while (len) {
+        ssize_t written = splice(infd, NULL, outfd, NULL, (size_t) len, SPLICE_F_MOVE);
+        if (written <= 0) {
+            return false;
+        }
+
+        len -= written;
+    }
+
+    return true;
+}
+
+_Noreturn static void usage() {
+    puts("Usage: tee <filename>");
+    exit(EXIT_FAILURE);
+}
+
+static mode_t mode(int fd) {
+    struct stat st;
+
+    if (fstat(fd, &st) == -1) {
+        exit(EXIT_FAILURE);
+    }
+
+    return st.st_mode;
+}
+
+/* return a string representation of the fd type */
+static const char *typestr(mode_t m) {
+    if (S_ISFIFO(m)) { return "pipe"; }
+    else if (S_ISREG(m)) { return "file"; }
+    else if (S_ISDIR(m)) { return "dir"; }
+    else if (S_ISBLK(m)) { return "special block file (device)"; }
+    else if (S_ISCHR(m)) { return "tty"; }
+    else if (S_ISSOCK(m)) { return "socket"; }
+    return "unknown";
+}
+
+/* fully generalized tee */
+static ssize_t ctee(int in, int out, int file) {
+    int inpipe[2];
+    int outpipe[2];
+
+    /* create the kernel buffers */
+    if (pipe(inpipe) < 0) {
+        perror("pipe()");
+        return -1;
+    }
+
+    if (pipe(outpipe) < 0) {
+        perror("pipe()");
+        goto parterror;
+    }
+
+    ssize_t written = 0;
+    do {
+        /* copy the input to the kernel buffer, passing SIZE_MAX or even
+         * SSIZE_MAX doesn't (always) work. The call returns "Invalid
+         * argument" */
+        ssize_t rcvd = splice(in, NULL, inpipe[1], NULL, (size_t) INT_MAX, SPLICE_F_MOVE);
+        if (rcvd == -1) {
+            perror("input splice()");
+            goto error;
+        }
+
+        if (rcvd == 0) {
+            /* reached the end of the input file */
+            break;
+        }
+
+#ifdef VERBOSE
+        printf("recvd %zd bytes\n", rcvd);
+#endif
+
+        /* "copy" to temporary buffer */
+        ssize_t teed = tee(inpipe[0], outpipe[1], (size_t) rcvd, SPLICE_F_NONBLOCK);
+        if (teed == -1) {
+            /* TODO:  this is definitely wrong in this case */
+            if (errno == EAGAIN) {
+                usleep(1000);
+                continue;
+            }
+            perror("tee()");
+            goto error;
+        }
+
+        if (teed == 0) {
+            /* we're done! */
+            break;
+        }
+
+#ifdef VERBOSE
+        printf("teed %zd bytes\n", teed);
+#endif
+
+        /* stream len bytes to `out` and `file` */
+        if (!spliceall(outpipe[0], file, teed)) {
+            perror("file splice()");
+            goto error;
+        }
+
+        if (!spliceall(inpipe[0], out, teed)) {
+            perror("out splice()");
+            goto error;
+        }
+
+        written += rcvd;
+    } while (1);
+
+    return written;
+
+error:
+    close(outpipe[0]);
+    close(outpipe[1]);
+parterror:
+    close(inpipe[0]);
+    close(inpipe[1]);
+
+    return -1;
+}
+
+static ssize_t ttee(int pin, int pout, int file) {
+    ssize_t written = 0;
+    do {
+        /* this will work if both stdin and stdout are pipes */
+        ssize_t len = tee(pin, pout, SSIZE_MAX, SPLICE_F_NONBLOCK);
+
+        if (len == -1) {
+            if (errno == EAGAIN) {
+                usleep(1000);
+                continue;
+            }
+            perror("tee()");
+            return -1;
+        }
+        else if (len == 0) {
+            /* we're done! */
+            break;
+        }
+
+        /* send the output to a file (consumes it) */
+        if (!spliceall(pin, file, len)) {
+            perror("splice():");
+            return -1;
+        }
+
+        written += len;
+    } while(1);
+
+    return written;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        usage();
+    }
+
+    int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd < 0) {
+        perror("couldn't create file");
+        exit(EXIT_FAILURE);
+    }
+
+    mode_t inmode = mode(STDIN_FILENO);
+    mode_t outmode = mode(STDOUT_FILENO);
+
+#ifdef VERBOSE
+    printf("STDIN is a %s!\n", typestr(inmode));
+    printf("STDOUT is a %s!\n", typestr(outmode));
+#endif
+
+    int status = EXIT_SUCCESS;
+    ssize_t written = -1;
+    /* detect which kind of file descript stdin and stdout are, some
+     * possibilities are: pipe, file, tty, ... */
+    if (S_ISFIFO(inmode) && S_ISFIFO(outmode)) {
+        /* both stdin and stdout are pipes (this happens in a shell when
+         * doing for example "... | utee | ..."), which means we don't have
+         * to create intermediate pipes */
+#ifdef VERBOSE
+        printf("taking shortcut\n");
+#endif
+        written = ttee(STDIN_FILENO, STDOUT_FILENO, fd);
+    }
+    else {
+        /* either stdin or stdout is not a pipe, so we use intermediate
+         * pipes to be able to use tee()/splice(), thus avoiding user-space
+         * buffers */
+#ifdef VERBOSE
+        printf("hard case...\n");
+#endif
+        written = ctee(STDIN_FILENO, STDOUT_FILENO, fd);
+    }
+
+    if (written == -1) {
+        status = EXIT_FAILURE;
+        goto end;
+    }
+
+#ifdef VERBOSE
+    printf("wrote %zd bytes\n", written);
+#endif
+
+end:
+    close(fd);
+    exit(status);
+}
